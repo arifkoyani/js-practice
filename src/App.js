66417b412954c7c140ea4310b55da2261229c6c5
arifkoyani <arifkoyani@gmail.com>
2024-04-03 06:33:23 -0700

    updated-animation-cmpnt

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
+import Bodyinimate from "./components/Bodyinimate";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
-  function addsallary(){
-    return console.log("this is your totalla Sallary")
+  function addsallary() {
+    return console.log("this is your totalla Sallary");
   }
   return (
     <>
-    <Navbar/>
-    <h3>Arif Hussain</h3>
-    <p>Hunza pakistan</p>
-    {addsallary()}
-    <Footer/>
+      <Navbar />
+      <h3>Arif Hussain</h3>
+      <p>Hunza pakistan</p>
+      {addsallary()}
+      <Bodyinimate/>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Bodyinimate.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+let Bodyinimate=()=>{
+    return(
+        <>
+        <div className="slide">
+
+        </div>
+        </>
+    )
+}
+export default Bodyinimate;
