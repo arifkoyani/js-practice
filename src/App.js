2fc2b086a1ebef28d3e8cba3229bb0a6afc7e0c1
arifkoyani <arifkoyani@gmail.com>
2024-04-03 05:38:04 -0700

    new code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import Navbar from "./components/Navbar";
 
 function App() {
+  function addsallary(){
+    return console.log("this is your totalla Sallary")
+  }
   return (
     <>
+    <Navbar/>
     <h3>Arif Hussain</h3>
+    <p>Hunza pakistan</p>
+    {addsallary()}
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+let Navbar=()=>{
+    return(
+        <>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Contact</li>
+                <li>FAQS</li>
+                <li>Cateloges</li>
+                <li>Gallery</li>
+            </ul>
+        </nav>
+        
+        </>
+    )
+}
+
+export default Navbar;
