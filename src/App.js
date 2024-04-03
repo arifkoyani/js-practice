88bff8aeaffc0015bc246cb1672184b4dc65a1ed
arifkoyani <arifkoyani@gmail.com>
2024-04-03 06:00:17 -0700

    finalcode

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <h3>Arif Hussain</h3>
     <p>Hunza pakistan</p>
     {addsallary()}
+    <Footer/>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+let Footer=()=>{
+    return(
+        <>
+        <footer>
+            <h2>This is My Footer  </h2>
+        </footer>
+        </>
+    )
+}
+export default Footer
