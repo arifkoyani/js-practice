82f388e7501c0f25243c1ed054eeb40e9bad4be3
arifkoyani <arifkoyani@gmail.com>
2024-04-03 10:07:47 -0700

    react-and-tailwindcss-integrated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,15 @@ import React from "react";
 let Navbar=()=>{
     return(
         <>
-        <nav>
-            <ul>
+        <nav> 
+            <ul className="justify-center items-center">
                 <li>Home</li>
                 <li>Contact</li>
                 <li>FAQS</li>
                 <li>Cateloges</li>
                 <li>Gallery</li>
             </ul>
+            <input className="outline border-[1px] outline-none rounded-[2px] "/>
         </nav>
         
         </>
